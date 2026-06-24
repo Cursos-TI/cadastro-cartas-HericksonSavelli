b204e1637a3b757d831b7d9e9eebc7bef8188213
HericksonSavelli <herickson.savelli11@gmail.com>
2026-06-24 10:08:34 +0000

    cadastro da segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -36,10 +36,12 @@ int main() {
     scanf("%f", &Pib);
     
     printf("Digite a quantidade de pontos turisticos da cidade: ");
-    scanf("%d\n\n", &pontosturisticos );
+    scanf("%d", &pontosturisticos );
+
+    printf("\n\n");
 
   // Área para exibição dos dados da cidade
-   printf("Carta 1:\n");
+    printf("Carta 1:\n");
     printf("Estado:%c\n", letra);
     printf("codigo:%c %s\n", letra, codigo);
     printf("Nome da cidade:%s\n", estado);
@@ -47,6 +49,43 @@ int main() {
     printf("Área:%.2f km²\n", area);
     printf("PIB:%.2f bilhões de reais\n", Pib);
     printf("Numero de pontos turisticos:%d\n\n", pontosturisticos);
+
+    // Área para entrada de dados
+    printf("--- CADASTRO DA CARTA 2 ---\n");
+
+    printf("Digite uma letra de A a H: ");
+    scanf(" %c", &letra);
+    
+    printf("Digite um numero de 01 a 04: ");
+    scanf("%s", codigo);
+
+    printf("Digite nome da cidade: ");
+    scanf("%s", estado);
+
+    printf("Digite numero de habitantes da cidade: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a area em quilometros quadrados: ");
+    scanf("%f", &area);
+
+    printf("Digite produto interno bruto: ");
+    scanf("%f", &Pib);
+
+    printf("Digite a quantidade de pontos turisticos da cidade: ");
+    scanf("%d", &pontosturisticos );
+
+    printf("\n\n");
+
+     // Área para exibição dos dados da cidade
+    printf("Carta 2:\n");
+    printf("Estado:%c\n", letra);
+    printf("codigo:%c %s\n", letra, codigo);
+    printf("Nome da cidade:%s\n", estado);
+    printf("População:%d\n", populacao);
+    printf("Área:%.2f km²\n", area);
+    printf("PIB:%.2f bilhões de reais\n", Pib);
+    printf("Numero de pontos turisticos:%d\n", pontosturisticos);
+    
     
 return 0;
 } 
