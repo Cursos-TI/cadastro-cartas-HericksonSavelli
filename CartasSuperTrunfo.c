5d1aa15f683a3120513d79c83a15cbaa10f18a7c
HericksonSavelli <herickson.savelli11@gmail.com>
2026-06-24 09:35:37 +0000

    cadastro da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,47 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+   char letra;
+    char codigo[4];
+    char estado[50];
+    int populacao;
+    float area;
+    float Pib;
+    int pontosturisticos;
 
   // Área para entrada de dados
+   printf("--- CADASTRO DA CARTA 1 ---\n");
+
+    printf("Digite uma letra de A a H: ");
+    scanf(" %c", &letra);
+    
+    printf("Digite um numero de 1 a 4: ");
+    scanf("%s", codigo);
+    
+    printf("Digite nome da cidade: ");
+    scanf("%s", estado);
+    
+    printf("Digite numero de habitantes da cidade: ");
+    scanf("%d", &populacao);
+    
+    printf("Digite a area em quilometros quadrados: ");
+    scanf("%f", &area);
+    
+    printf("Digite produto interno bruto: ");
+    scanf("%f", &Pib);
+    
+    printf("Digite a quantidade de pontos turisticos da cidade: ");
+    scanf("%d\n\n", &pontosturisticos );
 
   // Área para exibição dos dados da cidade
-
+   printf("Carta 1:\n");
+    printf("Estado:%c\n", letra);
+    printf("codigo:%c %s\n", letra, codigo);
+    printf("Nome da cidade:%s\n", estado);
+    printf("População:%d\n", populacao);
+    printf("Área:%.2f km²\n", area);
+    printf("PIB:%.2f bilhões de reais\n", Pib);
+    printf("Numero de pontos turisticos:%d\n\n", pontosturisticos);
+    
 return 0;
 } 
