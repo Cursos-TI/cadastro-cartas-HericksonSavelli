1e0f7a177d1dcc931d0a8cc51b54230990c9e8d2
HericksonSavelli <herickson.savelli11@gmail.com>
2026-06-27 10:22:59 +0000

    acrescentei informações no codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,8 @@ int main() {
     float area;
     float Pib;
     int pontosturisticos;
+    float densidade1, densidade2;
+    float PIBperCapita1, PIBperCapita2;
 
   // Área para entrada de dados
    printf("--- CADASTRO DA CARTA 1 ---\n");
@@ -38,17 +40,23 @@ int main() {
     printf("Digite a quantidade de pontos turisticos da cidade: ");
     scanf("%d", &pontosturisticos );
 
+    //Calculo de novas funções
+    densidade1 = populacao / area;
+    PIBperCapita1 = Pib / populacao;
+
     printf("\n\n");
 
   // Área para exibição dos dados da cidade
     printf("Carta 1:\n");
     printf("Estado:%c\n", letra);
-    printf("codigo:%c %s\n", letra, codigo);
+    printf("codigo:%c%s\n", letra, codigo);
     printf("Nome da cidade:%s\n", estado);
     printf("População:%d\n", populacao);
     printf("Área:%.2f km²\n", area);
     printf("PIB:%.2f bilhões de reais\n", Pib);
-    printf("Numero de pontos turisticos:%d\n\n", pontosturisticos);
+    printf("Numero de pontos turisticos:%d\n", pontosturisticos);
+     printf("Densidade População: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n\n", PIBperCapita1);
 
     // Área para entrada de dados
     printf("--- CADASTRO DA CARTA 2 ---\n");
@@ -74,6 +82,10 @@ int main() {
     printf("Digite a quantidade de pontos turisticos da cidade: ");
     scanf("%d", &pontosturisticos );
 
+    //Calculo de novas funções
+    densidade2 = populacao / area;
+    PIBperCapita2 = Pib / populacao;
+
     printf("\n\n");
 
      // Área para exibição dos dados da cidade
@@ -85,6 +97,8 @@ int main() {
     printf("Área:%.2f km²\n", area);
     printf("PIB:%.2f bilhões de reais\n", Pib);
     printf("Numero de pontos turisticos:%d\n", pontosturisticos);
+     printf("Densidade População: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", PIBperCapita2);
     
     
 return 0;
